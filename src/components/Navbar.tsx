d800b07f2ff79760e388cd31ba750a6643912cd8
Jetskiis <yegene60@gmail.com>
2023-02-06 22:06:31 -0500

    add dynamic calculation of current season/year

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,39 +1,42 @@
 import { BsGear, BsMoon } from "react-icons/bs";
 import { MdOutlineViewWeek } from "react-icons/md";
+import seasonInfo from "./Season";
 
 //navbar
 
 const Navbar = () => {
+  const year = seasonInfo.year;
   return (
-    <nav className=" bg-gray-700 px-5 py-6 text-white cursor-pointer">
+    <nav className=" cursor-pointer bg-gray-700 px-5 py-6 text-white">
       <div className="flex flex-row items-center justify-around ">
-        <h2 className="text-xl font-bold italic">AnimeTime</h2>
+        <h2 className="text-cyan-200 text-xl font-bold italic">AnimeTime</h2>
         <ul className="flex flex-row items-center justify-center text-lg font-medium">
           <li className="mx-3">
-            <a href="">Winter 2023</a>
+            <a href="">{seasonInfo.firstSeason} {year}</a>
           </li>
           <li className="mx-3 text-gray-300">
-            <a href="">Winter 2023</a>
+            <a href="">{seasonInfo.secondSeason} {year}</a>
           </li>
           <li className="mx-3 text-gray-300">
-            <a href="">Winter 2023</a>
+            <a href="">{seasonInfo.thirdSeason} {year}</a>
           </li>
           <li className="mx-3 text-gray-300">
-            <a href="">Winter 2023</a>
+            <a href="">{seasonInfo.fourthSeason} {year}</a>
           </li>
         </ul>
 
         <ul className="flex flex-row items-center justify-center text-sm">
-          <li className="flex flex-col mx-1.5">
-            <BsMoon className="" />
-            <br></br>
+          <li className="mx-1.5 flex flex-col items-center">
+            <BsMoon className="mb-0.5" />
+            {/* <br></br> */}
+            Dark
           </li>
-          <li className="flex flex-col items-center mx-1.5">
-            <MdOutlineViewWeek className="" />
+          <li className="mx-1.5 flex flex-col items-center">
+            <MdOutlineViewWeek className="mb-0.5" />
             Weekly
           </li>
-          <li className="flex flex-col items-center mx-1.5">
-            <BsGear className="" />
+          <li className="mx-1.5 flex flex-col items-center">
+            <BsGear className="mb-0.5" />
             Settings
           </li>
         </ul>
--- src/components/Season.jsx
+++ src/components/Season.jsx
@@ -1,4 +1,35 @@
-
-
-
+import { getYear, getMonth } from "date-fns";
 //calculates the season of anime
+
+//Winter: from January to March; Spring: from April to June; Summer: from July to September; and Fall: from October to December.
+let seasonInfo = {};
+
+const today = new Date();
+const year = getYear(today);
+const month = getMonth(today);
+
+if (month >= 0 && month <= 2) {
+    seasonInfo.firstSeason = "Winter";
+    seasonInfo.secondSeason = "Spring";
+    seasonInfo.thirdSeason = "Summer";
+    seasonInfo.fourthSeason = "Fall";
+} else if (month >= 3 && month <= 5) {
+    seasonInfo.firstSeason = "Spring";
+    seasonInfo.secondSeason = "Summer";
+    seasonInfo.thirdSeason = "Fall";
+    seasonInfo.fourthSeason = "Winter";
+} else if (month >= 6 && month <= 8) {
+    seasonInfo.firstSeason = "Summer";
+    seasonInfo.secondSeason = "Fall";
+    seasonInfo.thirdSeason = "Winter";
+    seasonInfo.fourthSeason = "Spring";
+} else if (month >= 9 && month <= 11) {
+    seasonInfo.firstSeason = "Fall";
+    seasonInfo.secondSeason = "Winter";
+    seasonInfo.thirdSeason = "Spring";
+    seasonInfo.fourthSeason = "Summer";
+}
+
+seasonInfo.year = year;
+
+export default seasonInfo;
